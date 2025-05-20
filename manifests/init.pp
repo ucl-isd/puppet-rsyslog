005d1bd39e6f6d2708d2f68a206d374b6ea34c79
Mike Woods <mike@geofront.co.uk>
2025-05-20 14:53:05 +0100

    Dumbass

--- manifests/init.pp
+++ manifests/init.pp
@@ -213,7 +213,7 @@ class rsyslog (
   }
 
   if($rsyslog_default_use_caller_module == true ) {
-    Notify("Caller module ${caller_module_name} - cake")
+    Notify{"Caller module ${caller_module_name} - cake": }
     file { $rsyslog_default:
       ensure  => file,
       owner   => 'root',
@@ -223,7 +223,7 @@ class rsyslog (
       require => File[$rsyslog_conf],
     }
   } else {
-    Notify("Caller module ${module_name} - Not cake")
+    Notify{"Caller module ${module_name}": }
     file { $rsyslog_default:
       ensure  => file,
       owner   => 'root',
